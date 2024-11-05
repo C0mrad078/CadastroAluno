1703a97678208c707d76ab432a9bfd20c8191a7f
jhonatan <[jhonaatan81@gmail.com](mailto:jhonaatan81@gmail.com)>
2024-11-05 20:49:09 -0200

    Inicio projeto

---
+++ src/main/java/principal/ProjetoMvc.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package principal;
+
+/**
+ *
+ * @author Administrator
+ */
+public class ProjetoMvc {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
